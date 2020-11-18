ab47bb5eaa6a87b909bc5e3cf95b952b864b276f
unknown <fedebarraza@alumno.unlam.edu.ar>
2020-11-17 22:33:52 -0300

    [EVA3T-1] nuevo carousel de imagenes

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.hero-carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: '<img class="arrow left" src="img/hero/chevron-left-solid.svg" alt="Left Arrow">',
+        nextArrow: '<img class="arrow right" src="img/hero/chevron-right-solid.svg" alt="Right Arrow">'
+    });
+});
